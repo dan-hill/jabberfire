98951b16a04d59c98ada46f1cce59c46c9ab6879
Dan Hill <dan@danhill.us>
2015-01-24 21:26:42 -0600

    added base.html and an example child template to the hello world module of templates

--- web-src/app/dashboard/hello_world.py
+++ web-src/app/dashboard/hello_world.py
@@ -4,6 +4,5 @@ from jinja2 import TemplateNotFound
 hello_world = Blueprint('hello_world', __name__, template_folder='templates')
 
 @hello_world.route('/', defaults={'page': 'index'})
-@hello_world.route('/<page>')
 def show(page):
     return render_template('index.html')
