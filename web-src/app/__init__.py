f04ff3ca28042d4a2e72a19b7a785c1806f1deba
Dan Hill <dan@danhill.us>
2015-02-03 13:29:25 -0600

    added resources from template to static

--- web-src/app/__init__.py
+++ web-src/app/__init__.py
@@ -1,6 +1,8 @@
 # setup the app object
 from flask import Flask
-from app.dashboard.hello_world import hello_world
+from dashboard.hello_world import hello_world
+from auth.auth import auth
 
 app = Flask(__name__)
 app.register_blueprint(hello_world)
+app.register_blueprint(auth)
--- web-src/app/auth/auth.py
+++ web-src/app/auth/auth.py
@@ -1,1 +1,9 @@
-__author__ = 'dan'
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+auth = Blueprint('auth', __name__, template_folder='templates')
+
+
+@auth.route('/login', defaults={'page': 'index'})
+def show(page):
+    return render_template('login.html')
