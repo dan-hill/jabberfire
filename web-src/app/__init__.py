49dab32edd25b5e7127394bd80ea106bae3ad694
Dan Hill <dan@danhill.us>
2015-02-10 16:54:04 -0600

    added some configuration options

--- web-src/app/__init__.py
+++ web-src/app/__init__.py
@@ -3,6 +3,13 @@ from flask import Flask
 from dashboard.dashboard import dashboard
 from auth.auth import auth
 
+# App initialization
 app = Flask(__name__)
+
+# Blueprint registrations
 app.register_blueprint(dashboard)
 app.register_blueprint(auth)
+
+# Global configurations
+app.config['ORGANIZATION_SHORT_NAME'] = 'CCMH'
+app.config['ORGANIZATION_LONG_NAME'] = "Comanche County Memorial Hospital"
--- web-src/app/dashboard/dashboard.py
+++ web-src/app/dashboard/dashboard.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+from flask import current_app
 
 dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 
 @dashboard.route('/dashboard', defaults={'page': 'index'})
 def show(page):
-    return render_template('dashboard.html')
+    return render_template('dashboard.html',
+                           short_name=current_app.config['ORGANIZATION_SHORT_NAME'])
