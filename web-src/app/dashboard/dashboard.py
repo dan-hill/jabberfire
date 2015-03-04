d8243871b06ca04c7305be6773e0b5757b2c1905
Dan Hill <dan@danhill.us>
2015-03-03 21:38:49 -0600

    Move templates from the module level to a central application template directory

--- web-src/app/dashboard/dashboard.py
+++ web-src/app/dashboard/dashboard.py
@@ -6,5 +6,5 @@ dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 
 @dashboard.route('/dashboard', defaults={'page': 'index'})
 def show(page):
-    return render_template('dashboard.html',
+    return render_template('../templates/dashboard.html',
                            short_name=current_app.config['ORGANIZATION_SHORT_NAME'])
