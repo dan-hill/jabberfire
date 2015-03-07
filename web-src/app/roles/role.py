c5c652d2396a8156471fb51970426f49d901990a
Dan Hill <dan@danhill.us>
2015-03-06 21:31:36 -0600

    Add role data model documentation

--- web-src/app/roles/role.py
+++ web-src/app/roles/role.py
@@ -1,7 +1,24 @@
 from app import db
 from flask_security import RoleMixin
 
+
 class Role(db.Model, RoleMixin):
+    """The user role data model.
+
+        User roles are defined when the application is initialized. To add
+        additional roles, insert them into the database from the
+        ``app.create_app()`` function using the ``user_datastore.create_role()``
+        function.
+
+    """
+
+    #: Generated id for the role.
     id = db.Column(db.Integer(), primary_key=True)
+
+    #: The name of the role.
+    #: This attribute is used when checking for user roles using the
+    #: ``@role_required`` decorator.
     name = db.Column(db.String(80), unique=True)
+
+    #: Short description of the role.
     description = db.Column(db.String(255))
--- web-src/docs/models.rst
+++ web-src/docs/models.rst
@@ -3,8 +3,18 @@ Data Models
 
 User
 ----
+
 .. module:: app.users.user
 .. autoclass:: User
+:members:
+        :undoc-members:
+        :show-inheritance:
+
+Role
+----
+
+.. module:: app.roles.role
+.. autoclass:: Role
 :members:
         :undoc-members:
         :show-inheritance:
