494a63fc0b0adb62e730ecd07763a91bff5534ef
Dan Hill <dan@danhill.us>
2015-03-30 23:17:30 -0500

    More mishteeve

--- app/controllers/user-management.js
+++ app/controllers/user-management.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     clickedThis: function(){
-      var userlist = this.model
+      var userlist = this.get('model');
       console.log(userlist)
     }
   }
