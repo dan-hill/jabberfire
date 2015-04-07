1a0c3da9040cb2ab80f3a35996ef1a413f397001
Dan Hill <dan@danhill.us>
2015-04-07 13:33:22 -0500

    Add powerd by.. logo

--- app/views/assets.js
+++ app/views/assets.js
@@ -3,11 +3,9 @@ import Ember from 'ember';
 export default Ember.View.extend({
   templateName: 'assets',
   didInsertElement: function() {
-    Ember.run.schedule('afterRender', function(){
       var menu_item = Ember.$('#assets-navigation-item');
       menu_item.addClass('open active');
       menu_item.find('#menu-item-decorator').addClass('selected');
-    })
   },
   willDestroyElement: function() {
     var menu_item = Ember.$('#assets-navigation-item');
--- app/views/suppliers.js
+++ app/views/suppliers.js
@@ -3,11 +3,9 @@ import Ember from 'ember';
 export default Ember.View.extend({
   templateName: 'suppliers',
   didInsertElement: function() {
-    Ember.run.schedule('afterRender', function(){
       var menu_item = Ember.$('#suppliers-navigation-item');
       menu_item.addClass('open active');
       menu_item.find('#menu-item-decorator').addClass('selected');
-    })
   },
   willDestroyElement: function() {
     var menu_item = Ember.$('#suppliers-navigation-item');
