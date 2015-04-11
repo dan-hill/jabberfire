fe4c3e33cbdc0e71ed6c658a5d5e420f7cd0b58a
Dan Hill <dan@danhill.us>
2015-04-11 12:35:54 -0500

    Remove variable fields from asset model

--- app/assets/model.py
+++ app/assets/model.py
@@ -6,49 +6,27 @@ class Asset(db.Model):
     def __init__(self, **kwargs):
         # self.id = kwargs.get('id')
         self.description = kwargs.get('description')
-        self.title = kwargs.get('title')
         self.serial = kwargs.get('serial')
         self.model = kwargs.get('model')
-        self.status = kwargs.get('status')
-        self.quantity_on_hand = kwargs.get('quantity_on_hand')
         self.max_quantity = kwargs.get('max_quantity')
         self.min_quantity = kwargs.get('min_quantity')
-        self.tag = kwargs.get('tag')
         self.image = kwargs.get('image')
         self.note = kwargs.get('note')
-        self.purchase_cost = kwargs.get('purchase_cost')
-        self._warranty_expiration = kwargs.get('warranty_expiration')
-        self._end_of_life = kwargs.get('end_of_life')
-        self.requestable = kwargs.get('requestable')
+
 
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(1000))
-    title = db.Column(db.String(255))
     serial = db.Column(db.String(255))
     model = db.Column(db.String(255))
-    status = db.Column(db.String(255))
-    quantity_on_hand = db.Column(db.Integer)
     max_quantity = db.Column(db.Integer)
     min_quantity = db.Column(db.Integer)
     tag = db.Column(db.Integer)
     image = db.Column(db.String(255))
     note = db.Column(db.String(1000))
-    purchase_cost = db.Column(db.Float)
-    _warranty_expiration = db.Column(db.Date)
-    _end_of_life = db.Column(db.Date)
-    requestable = db.Column(db.Boolean)
-    parent_id = db.Column(db.Integer, db.ForeignKey('asset.id'))
 
     manufacturer = db.Column(db.Integer, db.ForeignKey('manufacturer.id'))
 
     supplier = None
-
-    components = db.relationship(
-        'Asset',
-        cascade='all',
-        backref=db.backref(
-            'parent',
-            remote_side='Asset.id'))
 
     @property
     def warranty_expiration(self):
---
+++ app/units/model.py
@@ -0,0 +1,68 @@
+from app import db
+
+class Unit(db.Model):
+
+    def __init__(self, **kwargs):
+        self.status = kwargs.get('status')
+        self.tag = kwargs.get('tag')
+        self.image = kwargs.get('image')
+        self.note = kwargs.get('note')
+        self.purchase_cost = kwargs.get('purchase_cost')
+        self._warranty_expiration = kwargs.get('warranty_expiration')
+        self._end_of_life = kwargs.get('end_of_life')
+        self.requestable = kwargs.get('requestable')
+
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(255))
+    tag = db.Column(db.Integer)
+    image = db.Column(db.String(255))
+    note = db.Column(db.String(1000))
+    purchase_cost = db.Column(db.Float)
+    _warranty_expiration = db.Column(db.Date)
+    _end_of_life = db.Column(db.Date)
+    requestable = db.Column(db.Boolean)
+    parent_id = db.Column(db.Integer, db.ForeignKey('unit.id'))
+    asset_id = db.Column(db.Integer, db.ForeignKey('asset.id'))
+
+    asset = db.relationship(
+        'Asset',
+        cascaade='all',
+        backref=db.backref(
+            'asset',
+            remote_side='asset.id'
+        )
+    )
+
+    supplier = None
+
+    components = db.relationship(
+        'Unit',
+        cascade='all',
+        backref=db.backref(
+            'parent',
+            remote_side='unit.id'))
+
+    @property
+    def warranty_expiration(self):
+        return self._warranty_expiration.isoformat()
+
+    @property
+    def end_of_life(self):
+        return self._end_of_life.isoformat()
+
+
+    @staticmethod
+    def list():
+        """
+        Returns:
+          List containing objects for all users in the database.
+        """
+        return db.session.query(Unit).all()
+
+    @staticmethod
+    def find(**kwargs):
+        return db.session.query(Unit).filter_by(**kwargs).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
