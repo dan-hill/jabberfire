fc793a797139337c91e799727a55d74e7ee1154a
Dan Hill <dan@danhill.us>
2015-04-18 22:13:26 -0500

    Fix ember import

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    secret: grunt.file.readJSON('secret.json'),
+    compress: {
+      main: {
+        options: {
+          archive: 'deploy.tar.gz',
+          mode: 'tgz'
+        },
+        files: [
+          {src: ['dist/*'], 'dest': '/'}
+        ]
+      }
+    },
+    sftp: {
+      main: {
+        files: {
+          './': 'deploy.tar.gz'
+        },
+        options: {
+          path: '/home/dan/temp',
+          host: '<%= secret.host %>',
+          username: '<%= secret.username %>',
+          password: '<%= secret.password %>'
+        }
+      }
+    },
+    sshexec: {
+      extract: {
+        command: [
+          'tar xvfz /home/dan/temp/deploy.tar.gz -C /home/dan/temp',
+          'mv /home/dan/temp/dist/* /home/dan/www/client'
+        ],
+          options: {
+          host: '<%= secret.host %>',
+          username: '<%= secret.username %>',
+          password: '<%= secret.password %>'
+        }
+      },
+      clean: {
+        command: 'rm -rf /home/dan/temp/deploy.tar.gz',
+        options: {
+          host: '<%= secret.host %>',
+          username: '<%= secret.username %>',
+          password: '<%= secret.password %>'
+        }
+      }
+    },
+    clean: ['deploy.tar.gz'],
+    exec: {
+      deploy: {
+        cmd: function () {
+          /* I like to know when things finish :) */
+          return 'echo \'Deployment complete.\'';
+        }
+      },
+      build_production: {
+        cmd: function () {
+          /* I like to know when things finish :) */
+          return 'ember build --environment="production"';
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-ssh');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-exec');
+  // Default task(s).
+  grunt.registerTask('deploy', ['exec:build_production', 'compress', 'sftp', 'sshexec', 'clean', 'exec:deploy']);
+
+};
--- app/components/jf-portlet-icon.js
+++ app/components/jf-portlet-icon.js
@@ -1,3 +1,5 @@
+import Ember from 'ember';
+
 export default Ember.Component.extend({
   tagName: 'i',
   classNames: ['icon-screen-desktop','font-green-sharp']
